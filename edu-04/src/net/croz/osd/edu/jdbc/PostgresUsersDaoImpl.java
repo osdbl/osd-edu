2f9d5d85cdf79028da9af8b9f8db45a5657f14a4
Zoran Nikolic <znikolic@croz.net>
2016-09-23 14:51:48 +0200

    Database connection optimization.

--- edu-04/src/net/croz/osd/edu/jdbc/PostgresUsersDaoImpl.java
+++ edu-04/src/net/croz/osd/edu/jdbc/PostgresUsersDaoImpl.java
@@ -24,74 +24,80 @@ public class PostgresUsersDaoImpl implements UsersDao {
 	@Autowired
 	DataSource dataSource;
 
+	Connection c;
+
 	@Override
 	public List<User> getUsers() {
 		List<User> users = new ArrayList<>();
-		Connection c = null;
-		try {
-			c = dataSource.getConnection();
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-		}
+		getConnection();
 		Statement stmt = null;
+		ResultSet rs = null;
 		try {
 			stmt = c.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM users;");
+			rs = stmt.executeQuery("SELECT * FROM users;");
 			while (rs.next()) {
 				String username = rs.getString("username");
 				String password = rs.getString("password");
 				boolean enabled = rs.getBoolean("enabled");
-				User user=new User(username, password, enabled, null);
+				User user = new User(username, password, enabled, null);
 				List<UserRole> roles = getUserRoles(user);
 				Set<UserRole> set = new HashSet<UserRole>(roles);
 				user.setRoles(set);
 				users.add(user);
 			}
-			rs.close();
-			stmt.close();
-			c.close();
+
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				c.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+
+			}
 		}
+
 		return users;
 	}
 
 	@Override
 	public List<UserRole> getUserRoles(User user) {
-		Connection c = null;
-		try {
-			c = dataSource.getConnection();
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-		}
+
 		List<UserRole> roles = new ArrayList<UserRole>();
 		Statement stmt = null;
+		ResultSet rs = null;
 		try {
 			stmt = c.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT authority FROM authorities WHERE username='" + user.getUsername() + "';");
+			rs = stmt.executeQuery("SELECT authority FROM authorities WHERE username='" + user.getUsername() + "';");
 			while (rs.next()) {
 				String role = rs.getString("authority");
-				UserRole userRole=new UserRole(user,role);
+				UserRole userRole = new UserRole(user, role);
 				roles.add(userRole);
 			}
-			rs.close();
-			stmt.close();
-			c.close();
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+
+			}
 		}
+
 		return roles;
 	}
 
 	@Override
 	public void addUser(User user) {
-		Connection c = null;
-		try {
-			c = dataSource.getConnection();
-			c.setAutoCommit(false);
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-		}
+		
+		getConnection();
+
 		Statement stmt = null;
 		try {
 			stmt = c.createStatement();
@@ -99,25 +105,28 @@ public class PostgresUsersDaoImpl implements UsersDao {
 					+ user.isEnabled() + "');";
 			stmt.executeUpdate(sql);
 			c.commit();
-			if(user.getRoles()!=null)
+			if (user.getRoles() != null)
 				addUserRoles(user);
 			c.commit();
-			stmt.close();
-			c.close();
+
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		} finally {
+			try {
+				stmt.close();
+				c.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+
+			}
 		}
 	}
 
 	@Override
 	public void updateUser(User oldUser, User newUser) {
-		Connection c = null;
-		try {
-			c = dataSource.getConnection();
-			c.setAutoCommit(false);
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-		}
+		
+		getConnection();
+
 		Statement stmt = null;
 		try {
 			stmt = c.createStatement();
@@ -136,85 +145,108 @@ public class PostgresUsersDaoImpl implements UsersDao {
 			c.commit();
 			addUserRoles(newUser);
 			c.commit();
-			System.out.println("Operation done successfully");
 
-			stmt.close();
-			c.close();
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
+
+		} finally {
+			try {
+				stmt.close();
+				c.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+
+			}
 		}
 	}
 
 	@Override
 	public void deleteUser(User user) {
-		Connection c = null;
-		try {
-			c = dataSource.getConnection();
-			c.setAutoCommit(false);
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-		}
+
+		getConnection();
 		Statement stmt = null;
 		try {
 			stmt = c.createStatement();
 			String sql = "DELETE from users where username='" + user.getUsername() + "';";
 			stmt.executeUpdate(sql);
 			c.commit();
-			stmt.close();
-			c.close();
+
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		} finally {
+			try {
+				stmt.close();
+				c.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+
+			}
 		}
 	}
 
 	@Override
 	public void addUserRoles(User user) {
-		Connection c = null;
-		try {
-			c = dataSource.getConnection();
-			c.setAutoCommit(false);
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-		}
+
 		Statement stmt = null;
 		try {
 			stmt = c.createStatement();
 			List<UserRole> list = new ArrayList<UserRole>(user.getRoles());
 			for (int i = 0; i < list.size(); i++) {
-				if(list.get(i).getRole()!=""){
-				String sql = "INSERT INTO authorities VALUES ('" + user.getUsername() + "','" + list.get(i).getRole() + "');";
-				stmt.executeUpdate(sql);
-				c.commit();
+				if (list.get(i).getRole() != "") {
+					String sql = "INSERT INTO authorities VALUES ('" + user.getUsername() + "','"
+							+ list.get(i).getRole() + "');";
+					stmt.executeUpdate(sql);
+					c.commit();
 				}
 			}
-			stmt.close();
-			c.close();
+
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		} finally {
+			try {
+				stmt.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+
+			}
 		}
 	}
 
 	@Override
 	public void deleteUserRoles(User user) {
-		Connection c = null;
-		try {
-			c = dataSource.getConnection();
-			c.setAutoCommit(false);
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-		}
+
 		Statement stmt = null;
 		try {
 			stmt = c.createStatement();
 			String sql = "DELETE from authorities where username='" + user.getUsername() + "';";
 			stmt.executeUpdate(sql);
 			c.commit();
-			stmt.close();
-			c.close();
+			
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		} finally {
+			try {
+				stmt.close();
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+
+			}
 		}
+	}
+
+	public void getConnection() {
+
+		try {
+            
+			c = dataSource.getConnection();
+			c.setAutoCommit(false);
+
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+		}
+
 	}
 
 }
